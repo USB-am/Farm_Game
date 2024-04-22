599b286b2a050e0d47e5dcdc165b8a05e9f73dfc
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-04-22 17:47:09 +0300

    Restart project

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-# -*- coding: utf-8 -*-
+import logging
 
-import pygame
-
-from config import SCREEN
-from src.controllers.event_controllers import EventController
+from enemy import *
 
 
-pygame.init()
+# Set logging level
+logging.basicConfig(level=logging.INFO)
 
 
-class Application:
-	def __init__(self):
-		self.screen = pygame.display.set_mode(SCREEN.size)
-		pygame.display.set_caption('Stardew Valley')
-
-		self.timer = pygame.time.Clock()
-
-		self.event_controller = EventController(self.screen)
-
-	def run(self) -> None:
-		while self.event_controller.is_worked:
-			self.event_controller.check_event()
-
-			self.screen.fill((0, 0, 0))
-
-			self.event_controller.update()
-			self.event_controller.draw()
-
-			pygame.display.update()
-			self.timer.tick(60)
+def main():
+	es = [Goblin(hp=10) for i in range(10)]
+	e1 = Hero(name='Hero')
+	for e in es:
+		e1.attack(e)
 
 
 if __name__ == '__main__':
-	app = Application()
-	app.run()
+	main()
