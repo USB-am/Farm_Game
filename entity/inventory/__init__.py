b65f1a7684c4e85243225b4008d08297731d2a71
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-04-23 17:45:50 +0300

    Created InventoryElement

--- entity/inventory/__init__.py
+++ entity/inventory/__init__.py
@@ -1,12 +1,41 @@
+from typing import Type, Tuple, List
+
 from item import Item
 from exceptions import inventory as Exc
+
+
+class InventoryCell(list):
+	def __init__(self, obj_type: Type[Item]=None):
+		self.obj_type = obj_type
+
+	def append(self, element: Item) -> None:
+		''' Добавить элемент в ячейку '''
+
+		if self.obj_type is None:
+			self.obj_type = type(element)
+
+		if not isinstance(element, self.obj_type):
+			raise Exc.InventoryElementTypeError
+
+		super().append(element)
+
+	def __getitem__(self, count: int) -> Tuple[Item]:
+		if len(self) < count:
+			raise Exc.InventoryElementCountError
+
+		output = self[:count]
+		self = self[count:]
+		return output
+
+	def is_empty(self) -> bool:
+		return len(self) == 0
 
 
 class Inventory(list):
 	''' Инвентарь '''
 
-	def __init__(self, *elements: Item, size: int=10):
-		super().__init__(elements)
+	def __init__(self, size: int=10):
+		super().__init__()
 		self._size = size
 
 	@property
--- exceptions/inventory.py
+++ exceptions/inventory.py
@@ -1,8 +1,10 @@
 class InventoryFilledError(Exception):
 	''' Ошибка перезаполнения инвентаря '''
-	pass
 
 
 class InventoryOverSizeError(Exception):
 	''' Ошибка присвоения числа больше max_value в ScaleProperty '''
-	pass
+
+
+class InventoryElementCountError(Exception):
+	''' Ошибка при попытке получения количества элементов ячейки инвентаря > имеющегося '''
