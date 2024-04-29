f29aea7c06b3ce240684a9df1927402806dfb706
USB-am <smit.acer@mail.ru>
2024-04-29 16:54:48 +0300

    Created base objects for material things

---
+++ surroundings/__init__.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from typing import Tuple
+
+from pygame import Rect
+
+import settings
+from entity import TEntity
+
+
+class Object(ABC):
+	''' Преграждающий объект '''
+
+	def __init__(self, x: int, y: int, size: Tuple[int, int]=settings.BLOCK_SIZE):
+		self.rect = Rect((x, y, *size))
+
+	def collide(self, entity) -> bool:
+		return self.rect.colliderect(entity.rect)
+
+
+class FantomObject(Object):
+	''' Не преграждающий объект '''
+
+	def collide(self, entity) -> bool:
+		return False
