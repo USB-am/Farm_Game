faf894b40fbdd185f1b4f76ec296b0e2fe151e79
Цупко Роман Игоревич <smit.acer@mail.ru>
2024-05-02 17:05:22 +0300

    Added button to menu screen

--- screen_manager/menu.py
+++ screen_manager/menu.py
@@ -10,12 +10,18 @@ class Menu(Screen):
 	def __init__(self):
 		super().__init__(name='menu')
 
-		new_game_btn = field.Button('New Game', pygame.Rect(100, 50, 300, 140))
+		new_game_btn = field.Button('New Game', pygame.Rect(100, 25, 300, 140))
 		new_game_btn.bind(lambda: print('New Game'))
 
-		load_game_btn = field.Button('Load Game', pygame.Rect(100, 200, 300, 140))
+		load_game_btn = field.Button('Load Game', pygame.Rect(100, 165, 300, 140))
 		load_game_btn.bind(lambda: print('Load Game'))
-		self.add(new_game_btn, load_game_btn)
+
+		options_btn = field.Button('Options', pygame.Rect(100, 305, 300, 140))
+		options_btn.bind(lambda: print('Options'))
+
+		exit_btn = field.Button('Exit', pygame.Rect(100, 445, 300, 140))
+		exit_btn.bind(lambda: exit())
+		self.add(new_game_btn, load_game_btn, options_btn, exit_btn)
 
 	def draw(self, surface: pygame.Surface) -> None:
 		for sprite in self:
