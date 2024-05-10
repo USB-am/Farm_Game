986d158a9b14e4fb55f4cc9ef68c008b33ff48a7
USB-am <smit.acer@mail.ru>
2024-05-10 23:24:33 +0300

    Created a Map object

--- screen_manager/game/__init__.py
+++ screen_manager/game/__init__.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import pygame
 
 from screen_manager.screen import Screen
@@ -41,6 +43,63 @@ LEVEL = [
 ]
 
 
+BLOCKS = {
+	'#': Stone,
+}
+
+
+class Map(pygame.sprite.Group):
+	''' Карта '''
+
+	def __init__(self, map_list: List[str], **kwargs):
+		super().__init__(**kwargs)
+		self._map_list = map_list
+
+		self._init_map()
+
+	@property
+	def map_list(self) -> List[str]:
+		return self._map_list
+
+	@map_list.setter
+	def map_list(self, new_map: List[str]) -> None:
+		if not isinstance(new_map, list):
+			raise ValueError
+
+		self._map_list = new_map
+		self._init_map()
+
+	def _init_map(self) -> None:
+		''' Инициализировать объекты карты '''
+
+		self.empty()
+
+		for row, _ in enumerate(self.map_list):
+			for col, symbol in enumerate(_):
+				block_obj = BLOCKS.get(symbol)
+				if block_obj is None:
+					continue
+				pos_x = col * BLOCK_SIZE[0]
+				pos_y = row * BLOCK_SIZE[1]
+				self.add(block_obj(pos_x, pos_y))
+
+	@property
+	def size(self) -> Tuple[int]:
+		''' Максимальный размер карты '''
+
+		width = max([len(row) for row in self._map_list])
+		height = len(self._map_list)
+
+		return (width, height)
+
+	@property
+	def size_px(self) -> Tuple[int]:
+		''' Максимальный размер карты в пикселях '''
+
+		width, height = self.size
+		return (width * BLOCK_SIZE[0], height * BLOCK_SIZE[1])
+
+
 class Game(Screen):
 	''' Главный класс игры '''
 
@@ -48,16 +107,10 @@ class Game(Screen):
 		super().__init__(name='game', **kwargs)
 
 		self.target = Hero(200, 200, (30, 30), groups=self)
-		level_width = len(LEVEL[0]) * BLOCK_SIZE[0]
-		level_height = len(LEVEL) * BLOCK_SIZE[1]
-		self.camera = Camera(level_width, level_height)
+		self.map_ = Map(LEVEL)
+		self.add(*self.map_)
+		self.camera = Camera(*self.map_.size_px)
 		self.add(self.target)
-
-		# Temp
-		for row, _ in enumerate(LEVEL):
-			for col, simbol in enumerate(_):
-				if simbol == '#':
-					self.add(Stone(col * BLOCK_SIZE[0], row * BLOCK_SIZE[1]))
 
 	def keydown_event(self, event) -> None:
 		''' Обработка нажатия клавиши '''
