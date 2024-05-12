eadab47b32081309896591c00d6d7e7d28fdcb0c
USB-am <smit.acer@mail.ru>
2024-05-12 22:33:12 +0300

    Created Hero image update method

--- view/entity/hero.py
+++ view/entity/hero.py
@@ -13,16 +13,17 @@ class Hero(Entity):
 		self.groups = groups
 
 		self.up = self.down = self.left = self.right = False
+		self.direction = 'right'
 
 	def update(self) -> None:
-		if self.up:
-			self.yvel = -self.MOVE_SPEED
-		if self.down:
-			self.yvel = self.MOVE_SPEED
 		if self.left:
 			self.xvel = -self.MOVE_SPEED
 		if self.right:
 			self.xvel = self.MOVE_SPEED
+		if self.up:
+			self.yvel = -self.MOVE_SPEED
+		if self.down:
+			self.yvel = self.MOVE_SPEED
 
 		if not any((self.left, self.right)):
 			self.xvel = 0
@@ -34,8 +35,24 @@ class Hero(Entity):
 		self.rect.y  += self.yvel
 		super()._check_collide(self.groups)
 
-	def collide(self, *a, **kw):
+		self.update_image()
+
+	def collide(self, *a, **kw) -> bool:
 		return False
+
+	def update_image(self) -> None:
+		if self.left:
+			self.direction = 'left'
+			self.image.fill(pygame.Color('green'))
+		if self.right:
+			self.direction = 'right'
+			self.image.fill(pygame.Color('white'))
+		if self.up:
+			self.direction = 'up'
+			self.image.fill(pygame.Color('blue'))
+		if self.down:
+			self.direction = 'down'
+			self.image.fill(pygame.Color('red'))
 
 	def draw(self, surface: pygame.Surface) -> None:
 		surface.blit(self.image, (self.rect.x, self.rect.y))
